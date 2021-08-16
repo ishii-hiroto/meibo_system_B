c9ec75d1057ca1d1605366c9792457c5833232ae
sekine-kaho <sekine-kaho@webarge.jp>
2021-08-16 17:53:16 +0900

    [add] 新規ファイル登録　（社員情報詳細）
    #008 テーブルのみ完成　

---
+++ detail01.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <title>社員情報詳細</title>
+    </head>
+    <body>
+
+        <table>
+            <tr>
+                <td>社員名簿システム</td>
+                <td>| <a href="./index.php">トップ画面</a>
+                    | <a href="./entry01.php">新規社員登録へ</a> |
+                </td>
+            </tr>
+        </table>
+            <hr>
+
+        <table>
+            <tr>
+            <th>社員ID</th>
+                <td>
+
+                </td>
+            </tr>
+
+            <tr>
+            <th>名前</th>
+                <td>
+
+                </td>
+            </tr>
+
+            <tr>
+            <th>出身地</th>
+                <td>
+
+                </td>
+            </tr>
+
+            <tr>
+            <th>性別</th>
+                <td>
+
+                </td>
+            </tr>
+
+            <tr>
+            <th>年齢</th>
+                <td>
+
+                </td>
+            </tr>
+
+            <tr>
+            <th>所属部署</th>
+                <td>
+
+                </td>
+            </tr>
+
+
+        <td><form method='post' action='（遷移先のフォルダ）' style="text-align:right">
+            <input type="submit" value="編集" >  <!--編集あとで検索-->
+        <td><input type="reset" value="削除">
+    </tr>
+
+
+
+
+
+
+
+    </body>
+</html>
