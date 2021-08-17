8d5108d211b55157277b28c6c3bfcfbb24072cdc
KageyamaKyo <kageyama-kyo@webrage.jp>
2021-08-17 14:33:44 +0900

    [update]社員データをすべてDBから取り出してみる（トップ画面）
    #012 #017に移る前にプッシュしておきます。

--- index.php
+++ index.php
@@ -56,25 +56,45 @@
                     </select>
                 </td>
                 <tr>
-                    <td><input type="serch" value="検索"></td>
+                    <td><input type="submit" value="検索"></td>
                     <td><input type="reset" value="リセットする"></td>
                 </tr>
         </table>
         <hr>
+        <table border="1" style="border-collapse:collapse;">
+            <tr>
+                <th>社員ID</th>
+                <th>名前</th>
+                <th>部署</th>
+                <th>役職</th>
+            </tr>
         <?php
-            $DB_DSN = "mysql:host=localhost; dbname=hishi; charset=utf8";
+            $DB_DSN = "mysql:host=localhost; dbname=hishii; charset=utf8";
             $DB_USER = "webaccess";
             $DB_PW = "toMeu4rH";
             $pdo = new PDO($DB_DSN, $DB_USER, $DB_PW);
 
-
-            $query_str = "SELECT * FROM member WHERE LIKE % $_GET['name'] %";   //0のときどうする？for文
+            $query_str = "SELECT member.name, member.member_ID, grade_master.grade_name, section1_master.section_name FROM member
+                          LEFT JOIN section1_master ON section1_master.ID = member.section_ID
+                          LEFT JOIN grade_master ON grade_master.ID = member.grade_ID";   // 実行するSQL文を作成して変数に保持
 
             echo $query_str;                                   // 実行するSQL文を画面に表示するだけ（デバッグプリント
             $sql = $pdo->prepare($query_str);                  // PDOオブジェクトにSQLを渡す
             $sql->execute();                                   // SQLを実行する
             $result = $sql->fetchAll();                        // 実行結果を取得して$resultに代入する
 
-
+            foreach ($result as $x) {
+                echo "<tr><td style='text-align: right'>" . $x['member_ID'] . "</td>";
+                echo "<td>" . $x['name'] . "</td>";
+                echo "<td>" . $x['section_name'] . "</td>";
+                echo "<td >" . $x['grade_name'] . "</td></tr>";
+            }
+        ?>
+        </table>
+        <pre>
+            <?php
+                var_dump($result);
+            ?>
+        </pre>
     </body>
 </html>
