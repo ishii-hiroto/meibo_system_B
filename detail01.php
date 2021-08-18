85b79beaed9a3bd00d111c3454082d4c59e83644
sekine-kaho <sekine-kaho@webarge.jp>
2021-08-18 09:37:07 +0900

    test2

--- detail01.php
+++ detail01.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-    <head>test
+    <head>test2
         <meta charset='utf-8'>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
         <title>社員情報詳細</title>
