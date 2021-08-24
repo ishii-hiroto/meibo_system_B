777ada6edbe43680b201ba3655a01787c22d5803
ishii <ishii-hiroto@webrage.jp>
2021-08-24 11:25:25 +0900

    特になし

--- index.php
+++ index.php
@@ -1,63 +1,73 @@
 <!DOCTYPE html>
 <html>
-    <head>test2
+    <head>
         <meta charset='utf-8'>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
-        <title>社員情報詳細</title>
+        <title>社員情報検索</title>
     </head>
     <body>
+        <table>
             <tr>
                 <td>社員名簿システム</td>
                 <td>| <a href="./index.php">トップ画面</a>
                     | <a href="./entry01.php">新規社員登録へ</a> |
                 </td>
             </tr>
-            <hr>
+        </table>
+        <hr>
+
+        <form method="GET" action="./index.php" name='searchform'>
         <table>
             <tr>
-            <th>社員ID</th>
-                <td>
+                <td>名前：</td>
+                <td class>
+                    <input type="search" name="name" size="20" maxlength="30" placeholder="山田太郎" value="">
                 </td>
             </tr>
             <tr>
-            <th>名前</th>
+                <td>性別：</td>
                 <td>
+                    <select name="gender">
+                        <option value="" selected>すべて</option>
+                        <option value="1">男性</option>
+                        <option value="2">女性</option>
+                    </select>
                 </td>
-            </tr>
-            <tr>
-            <th>出身地</th>
+                <td>部署：</td>
                 <td>
+                    <select name="section">
+                        <option value="" selected>すべて</option>
+                        <option value="1">第一事業部</option>
+                        <option value="2">第二事業部</option>
+                        <option value="3">営業</option>
+                        <option value="4">総務</option>
+                        <option value="5">人事</option>
+                    </select>
                 </td>
-            </tr>
-            <tr>
-            <th>性別</th>
+                <td>役職：</td>
                 <td>
+                    <select name="grade">
+                        <option value="" selected>すべて</option>
+                        <option value="1">事業部長</option>
+                        <option value="2">部長</option>
+                        <option value="3">チームリーダー</option>
+                        <option value="4">リーダー</option>
+                        <option value="5">メンバー</option>
+                    </select>
                 </td>
-            </tr>
+                <tr>
+                    <td><input type="submit" value="検索"></td>
+                    <td><input type="reset" value="リセットする"></td>
+                </tr>
+        </table>
+        <hr>
+        <table border="1" style="border-collapse:collapse;">
             <tr>
-            <th>年齢</th>
-                <td>
-                </td>
+                <th>社員ID</th>
+                <th>名前</th>
+                <th>部署</th>
+                <th>役職</th>
             </tr>
-            <tr>
-            <th>所属部署</th>
-                <td>
-                </td>
-            </tr>
-            <tr>
-            <th>役職</th>
-                <td>
-                </td>
-            </tr>
-            <tr>
-            <td>
-                <input type="button" onclick="location.href='./update01.php'" value="編集">
-                <input type="reset" value="削除"
-                onclick="return confirm('削除ボタンがクリックされました。本当に入力内容を削除してもよろしいですか？');">
-            </td>
-            </tr>
-<<<<<<< HEAD
-=======
         <?php
             $DB_DSN = "mysql:host=localhost; dbname=hishii; charset=utf8";
             $DB_USER = "webaccess";
@@ -99,7 +109,7 @@
             $sql = $pdo->prepare($query_str);                  // PDOオブジェクトにSQLを渡す
             $sql->execute();                                   // SQLを実行する
             $result = $sql->fetchAll();                        // 実行結果を取得して$resultに代入する
-            echo "検索件数：" . count($result); 
+            echo "検索件数：" . count($result);
 
 
             $CNT = count($result);
@@ -109,20 +119,19 @@
             }else{
                 foreach ($result as $x) {
                     echo "<tr><td style='text-align: right'>" . $x['member_ID'] . "</td>";
-<<<<<<< HEAD
-                    echo "<td><a href='datail01.php?member_ID=".
-                                $x['member_ID']"'>".
-                                $x['name']."</a>" ."</td>";
-=======
                     echo "<td><a href='datail01.php?id=" .
                         $x['member_ID'] . "'>" .
                         $x['name'] . "</a></td>";
->>>>>>> 00dbd68e8cd48a1e2ed71ca2589f3bc0ed8b4121
                     echo "<td>" . $x['section_name'] . "</td>";
                     echo "<td >" . $x['grade_name'] . "</td></tr>";
                 }
             }
         ?>
         </table>
+        <pre>
+            <?php
+                var_dump($result);
+            ?>
+        </pre>
     </body>
 </html>
