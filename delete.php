facfe336468cbcb63a96fe50ee5dc32eada6b84c
ishii <ishii-hiroto@webrage.jp>
2021-08-25 12:07:46 +0900

    [add]詳細画面の削除処理を行うダミーデータの追加

---
+++ delete.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <title>新規社員情報登録ダミー画面</title>
+    </head>
+    <body>
+        <?php
+            $DB_DSN = "mysql:host=localhost; dbname=hishii; charset=utf8";
+            $DB_USER = "webaccess";
+            $DB_PW = "toMeu4rH";
+            $pdo = new PDO($DB_DSN, $DB_USER, $DB_PW);
+
+            $ID = $_POST["delete"];     //detail.phpのvalue値をidに渡す
+
+            $query_str = "DELETE FROM 'member' WHERE 'member'.'member_ID' = $ID";   // 実行するSQL文を作成して変数に保持
+
+            $sql = $pdo->prepare($query_str);     // PDOオブジェクトにSQLを渡す
+            $sql->execute();                      // SQLを実行する test5
+            $result = $sql->fetch();
+        ?>
+    </body>
+</html>
