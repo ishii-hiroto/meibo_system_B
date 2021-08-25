016ee8baf1ec72c6bf908ee9bf58d617b86e7e69
ishii <ishii-hiroto@webrage.jp>
2021-08-25 16:34:43 +0900

    [fix]削除後にtopページに遷移する処理文を修正しました。
    以前のコードだと遷移しないため

--- delete.php
+++ delete.php
@@ -13,11 +13,12 @@
             $pdo = new PDO($DB_DSN, $DB_USER, $DB_PW);
 
             $ID = $_POST["delete"];     //detail.phpのvalue値をidに渡す
-
-            $query_str = "DELETE FROM 'member' WHERE 'member'.'member_ID' = $ID";   // 実行するSQL文を作成して変数に保持
+            $query_str = "DELETE FROM member WHERE member.member_ID = $ID";   // 実行するSQL文を作成して変数に保持
 
             $sql = $pdo->prepare($query_str);     // PDOオブジェクトにSQLを渡す
-            $sql->execute();                      // SQLを実行する test5
+            if ($sql->execute()) {// SQLを実行する test5
+                header('Location:index.php');
+            }
             $result = $sql->fetch();
         ?>
     </body>
