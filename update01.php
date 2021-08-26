f156d0ce719e03a928e83967590212b37351c7e6
sekine-kaho <sekine-kaho@webarge.jp>
2021-08-27 08:03:52 +0900

    aaa

--- update01.php
+++ update01.php
@@ -6,39 +6,40 @@
         <title>社員情報詳細</title>
 
         <script type="text/javascript">
-            function check(){
-                var name01_flag=0;
-                var pref_flag=0;
-                var age01_flag=0;
-                var age01_value = document.mainform.age01.value;
+        function check(){
+        var name_flag=0;
+        var pref_flag=0;
+        var age_flag=0;
+        var age_value = document.mainform.age.value;
 
-                //入力判定
-                if(document.mainform.name01.value==""){
-                    name01_flag=1;
-                }else if(document.mainform.pref.value=="0"){
-                    pref_flag=1;
-                }else if(document.mainform.age01.value==""){
-                    age01_flag=1;
-                }
+        //入力判定
+        if(document.mainform.name.value==""){
+            name_flag=1;
+        }else if(document.mainform.pref.value=="0"){
+            pref_flag=1;
+        }else if(document.mainform.age.value==""){
+            age_flag=1;
+        }
 
-                //エラー文表示
-                if(name01_flag==1){
-                    window.alert('名前は必須です');
-                    return false;
-                }else if(pref_flag==1){
-                    window.alert('都道府県は必須です');
-                    return false;
-                }else if(age01_flag==1 || age01_value < 1 || age01_value > 100){
-                    window.alert('年齢は必須です');
-                    return false;
-                }else if(window.confirm('登録を行います。よろしいですか？')){
-                    location.href="../system/detail01.php";//「OK」の場合はindex.phpに移動
-                }else{
-                    windows.alert('キャンセルされました');//警告ダイアログ
-                    return false;//送信を中止
-                }
-            }
-        </script>
+        //エラー文表示
+        if(name_flag==1){
+            window.alert('名前は必須です');
+            return false;
+        }else if(pref_flag==1){
+            window.alert('都道府県は必須です');
+            return false;
+        }else if(age_flag==1 || age_value < 1 || age_value > 100){
+            window.alert('年齢は必須です');
+            return false;
+        }else if(window.confirm('登録を行います。よろしいですか？')){
+            // location.href="./entry02.php";//「OK」の場合はindex.phpに移動
+            document.mainform.submit();
+        }else{
+            windows.alert('キャンセルされました');//警告ダイアログ　テスト
+            return false;//送信を中止
+        }
+    }
+    </script>
     </head>
     <body>
             <td>社員名簿システム</td>
@@ -126,11 +127,18 @@
                         ?></td>
 
         </table>
-        <table>
+
+
+    
+
+
                 <tr>
                     <td><input type="button" onclick="check();" value="登録"></td>
                     <td><input type="reset" value="リセット"></td>
+                    <p style="text-align:right">
+                    <input type="reset">
+                </p>
                 </tr>
-        </table>
-    </body>
+    </script>
+</body>
 </html>
