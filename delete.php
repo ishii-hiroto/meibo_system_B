c00f8db3a41cc70030db20bba3d544a9f5fce835
sekine-kaho <sekine-kaho@webarge.jp>
2021-08-30 14:25:05 +0900

    特に変更なし

--- delete.php
+++ delete.php
@@ -16,10 +16,11 @@
             $query_str = "DELETE FROM member WHERE member.member_ID = $ID";   // 実行するSQL文を作成して変数に保持
 
             $sql = $pdo->prepare($query_str);     // PDOオブジェクトにSQLを渡す
-            if ($sql->execute()) {// SQLを実行する 
+            if ($sql->execute()) {// SQLを実行する
                 header('Location:index.php');
             }
             $result = $sql->fetch();
         ?>
     </body>
 </html>
+© 2021 GitHub, Inc.
